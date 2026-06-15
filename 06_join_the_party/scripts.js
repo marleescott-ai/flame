fe546ba7eea11f6a4ce905066376a7e9e94002ce
marleescott-ai <marleescott@shgcham.school.nz>
2026-06-15 12:48:38 +1200

    Lesson 6 (with Natalie)

--- 06_join_the_party/scripts.js
+++ 06_join_the_party/scripts.js
@@ -1,10 +1,14 @@
-let nameInput = document.getElementById("nameInput");
+let nameInput = document.getElementById("nameInput"); 
+let roleInput = document.getElementById("roleInput"); 
 let greetButton = document.getElementById("greetButton");
-let output = document.getElementById("output");
+let output = document.getElementById("output"); 
 
 function sayHello() {
-  let name = nameInput.value;
-  output.innerText = "Welcome, " + name;
+  let name = nameInput.value; //takes the name//
+  let role = roleInput.value;
+  let bank = bankInput.value;
+  let place = placeInput.value;
+  output.innerText = " Welcome, " + name + " the " + role + " to the Pattinson Party or PP for short. We'll be withdrawing $1000 every day from " + bank + " for your daily fee's. We will be sending spam mail to " + place + ". Have a Great day, We'll see you soon.😏👣🔥🥚 " ; //Makes the message//
 }
 
-greetButton.onclick = sayHello;
+greetButton.onclick = sayHello; //prints the prints the message on the page//
