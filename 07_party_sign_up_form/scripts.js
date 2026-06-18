511436485ac82c52ca0233ec4c2d4197e3d2e451
marleescott-ai <marleescott@shgcham.school.nz>
2026-06-19 09:59:13 +1200

    lesson 8

--- 07_party_sign_up_form/scripts.js
+++ 07_party_sign_up_form/scripts.js
@@ -4,9 +4,19 @@ function showSignupMessage() {
   let security = document.getElementById("securityInput").value
   let birthday = document.getElementById("birthInput").value
   let fear = document.getElementById("fearInput").value
-
-  let message = `Welcome offically to the party ${name}! \n This is the fine print of the Pattinson Party. \n Can we confirm this is your region ${region}? \n Can we confirm this is your soical security number ${security}? \n Can we confirm this is your birthday ${birthday} \n Can we confirm this is your fear? ${fear} \n Amazing! You are offically in the PP party! Now an acceptance letter will come by carrier pigeon in the next few months. \n Now Remember \n YOU CAN NEVER LEAVE NOW. \n We Know Your Every Move. \n We Know Everything About You \n And King Patti Is Hungry. \n So Don't Step Out Of Line Or You Might Find Yourself in the JAWS OF DOOM`
-  document.getElementById("output").innerText = message; 
+ 
+  let message 
+  if (name == ''){
+    message = 'Please enter Information'
+  } else if (region == ''){
+    message = 'Please enter Information'
+  } else {
+    message = `Welcome offically to the party ${name}! \n This is the fine print of the Pattinson Party. \n Can we confirm this is your region ${region}? \n Can we confirm this is your soical security number ${security}? \n Can we confirm this is your birthday ${birthday} \n Can we confirm this is your fear? ${fear} \n Amazing! You are offically in the PP party! Now an acceptance letter will come by carrier pigeon in the next few months. \n Now Remember \n YOU CAN NEVER LEAVE NOW. \n We Know Your Every Move. \n We Know Everything About You \n And King Patti Is Hungry. \n So Don't Step Out Of Line Or You Might Find Yourself in the JAWS OF DOOM`
+ 
 }
+document.getElementById("output").innerText = message; 
+  }
+
+ 
 
 document.getElementById("signupButton").onclick = showSignupMessage
--- 08_contribute_to_the_campaign/scripts.js
+++ 08_contribute_to_the_campaign/scripts.js
@@ -5,12 +5,12 @@ function giveTenDollars() {
 
   document.getElementById("totalDisplay").innerText = "Total raised: $" + totalDonations;
 
-  let message = "Thanks for your $10!";
+  let message = "Thanks for your $10! Queen Elj is still sad though.";
 
   if (totalDonations >= 50 && totalDonations < 100) {
-    message = "🎉 We've hit our first campaign milestone!";
+    message = "🎉 NICE! You might not end up in the JAWS OF DOOM... for now!";
   } else if (totalDonations >= 100) {
-    message = "🚀 Goal smashed! You powered this campaign!";
+    message = "🚀 YOU DID IT! King Patti is pleased!";
   }
 
   document.getElementById("thankYouMessage").innerText = message;
@@ -18,3 +18,20 @@ function giveTenDollars() {
 
 document.getElementById("giveButton").onclick = giveTenDollars
 
+
+function takeTenDollars() {
+  totalDonations = totalDonations - 10;
+
+  let message = "HOW DARE YOU! THE JAWS OF DOOM AWAIT YOUR TRIAL!.";
+
+if (totalDonations < 0) {
+  totalDonations = 0
+}
+
+ document.getElementById("totalDisplay").innerText = "Total raised: $" + totalDonations;
+ 
+  document.getElementById("thankYouMessage").innerText = message;
+  document.getElementById('woody')['src'] = './woody.jpg'
+ }
+
+document.getElementById("refundButton").onclick = takeTenDollars
